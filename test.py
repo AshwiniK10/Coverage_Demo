eb2d7b6be814efcf87a06bd5271c034c31b53967
AshwiniK10 <35912753+AshwiniK10@users.noreply.github.com>
2020-03-06 09:45:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+import unittest
+from palindromCheck import palindromCheck
+# import palindromeCheck
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(palindromCheck('abc'), 0),
+        self.assertEqual(palindromCheck('121'), 1)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
 
